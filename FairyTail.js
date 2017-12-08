032b0ebb07e1ab7f048c5526e335188b5357eaa8
koragen <sol_kot@list.ru>
2017-12-08 20:29:29 +0400

    khe khe

--- FairyTail.js
+++ FairyTail.js
@@ -9,7 +9,7 @@ var handlebars = require('express-handlebars').create({ defaultLayout:'main' });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3020);
 
 app.use(express.static(__dirname + '/public'));
 
@@ -37,7 +37,6 @@ app.get('/album', function (req, res) {
 app.get('/event', function (req, res) {
   res.render('event');
 });
-
 
 //Пользовательская страница 404
 app.use(function (req, res) {
